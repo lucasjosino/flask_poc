6eec185fb36be3b44af3e91232acc08a030410b8
lucasjosino <lukasj@live.com>
2020-05-10 22:26:44 -0300

    atualizando configurações da porta e do host para o heroku

--- app.py
+++ app.py
@@ -215,5 +215,6 @@ def cadastraModelo():
     except:
         return Response(status = 500)
 
-if __name__ == '__main__':
-    app.run(debug = True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
